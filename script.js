fbb1f891b486ea337d472d713aab984d18d8baf5
Hentony luan... <fallluan0@gmail.com>
2024-06-05 14:37:41 -0300

    playRockPaperScissor; foi adicionado

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-/* Desenvolva seu código abaixo */
+function playRockPaperScissor(player1, player2) {
+    if (player1 === player2) {
+        return "Empate!";
+    }
+
+    if ((player1 === "Pedra" && player2 === "Tesoura") ||
+        (player1 === "Tesoura" && player2 === "Papel") ||
+        (player1 === "Papel" && player2 === "Pedra")) {
+        return "Jogador 1 venceu!";
+    } else {
+        return "Jogador 2 venceu!";
+    }
+}
+
+
+const resultado = playRockPaperScissor("Pedra", "Tesoura");
+console.log(resultado);
+
